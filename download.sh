3d63bb73f7f13c0fb171c5705ab4f4ec99a1f535
kimown <kimown@163.com>
2020-12-03 16:29:57 +0800

    chore:

--- download.sh
+++ download.sh
@@ -1,2 +1,2 @@
-wget https://github.com/Kitware/CMake/releases/download/v3.15.2/cmake-3.15.2-Linux-x86_64.tar.gz
+wget https://cmake.org/files/v3.15/cmake-3.15.2-Linux-x86_64.tar.gz
 git clone --depth 1 https://github.com/KhronosGroup/Vulkan-ValidationLayers.git
